806e9cd132d391576dbae5f597ee64426c5cb911
Madhurgupta12 <om96002@gmail.com>
2024-05-30 00:18:09 +0530

    Complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,14 @@ import Carousel from './Carousel'
 const App = () => {
   return (
     <>
+    <div className="bg-gradient-to-r from-blue-500 to-red-500 min-h-screen overflow-y-auto">
+      <div className="flex justify-center text-3xl">
+        Railway-Collie
+      </div>
+    <Carousel></Carousel>
+    </div>
 
-   <img src="./coolie2.png" alt="hello" />
+  
     </>
     
   )
--- client/src/Carousel.js
+++ client/src/Carousel.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './style.css'; // Import your CSS file for styling
-
-const images = ["./coolie2.png"]; // Paths to your images
+import i1 from "./Image/4503642.jpg"
+import i2 from "./IMG-20231223-WA0017.jpg"
+import i3 from "./leftKNU.png"
+import i4 from "./IMG-20231223-WA0025.jpg"
+import i5 from "./coolie2.png"
+const images = [i1,i3,i4,i5]; // Paths to your images
 
 const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -17,13 +21,13 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="slideshow-container">
+    <div className="h-100 w-100 flex justify-center items-center">
       {images.map((image, index) => (
         <div
           key={index}
           className={index === currentIndex ? 'slide active' : 'slide'}
         >
-          <img src="./logo.svg" alt={`Slide ${index + 1}`} />
+          <img className="h-100 w-50 object-contain" src={image} alt={`Slide ${index + 1}`} />
         </div>
       ))}
     </div>
